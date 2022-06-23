51bb191e06eb0f8b761b38798bcb633e73d25d28
NienDev <107404601+NienDev@users.noreply.github.com>
2022-06-23 18:28:47 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,53 +1,121 @@
+import requests
 import socket
+import json
+import pickle
+from io import BytesIO
+import os
+from tkinter import *
+from PIL import ImageTk, Image
 
-HOST = "127.0.0.1" #loopback (point to my laptop IP adđress
-SERVER_PORT = 65432 # > 50000
-FORMAT = "utf8" 
+HOST = "127.0.0.1"
+PORT = 65432
+FORMAT = "utf8"
 
-def clientChat(client):
-    list = ["nien", "kim", "tran"]
-    msg = None
-    while msg != "x":
-        msg = input("message: ")
-        client.sendall(msg.encode(FORMAT))
-        msg = client.recv(1024).decode(FORMAT)
-        print("server message: ", msg)
-        if (msg == "list"): 
-            sendList(list, client)
+def show_menu(logo, welcome_label, btn, client, img_labels):
+    global FOOD_LISTS
+    global root
+    #turn off the show_welcome window
+    logo.forget()
+    welcome_label.forget()
+    btn.forget()
+    
+    #show menu
+    for img_label in img_labels:
+        img_label.pack()
+    
+def download_food_image(Food_Info):
+    #create folder
+    current_directory = os.getcwd()
+    final_directory = os.path.join(current_directory, r'food_imgs')
+    if not os.path.exists(final_directory):
+       os.makedirs(final_directory)
+       
+       for idx, info in enumerate(Food_Info):
+        # print(pic_url)
+        with open('./food_imgs/pic' + str(idx) + '.jpg', 'wb') as handle:
+            response = requests.get(info['url'], stream=True)
+
+            if not response.ok:
+                print(response)
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+
+                handle.write(block)       
+    
+def show_welcome(logo, frame, welcome_label, btn):
+    global root
+    
+    frame.pack(padx=50, pady=50)
+    
+    logo.pack()
+    
+    welcome_label.pack(pady=(20,40))
+    
+    btn.pack(pady=(0,20))
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("CLIENT SIDE")
-
-def sendList(list, client):
-    for item in list:
-        client.sendall(item.encode(FORMAT))
-        client.recv(1024)
-        
-    #finish sending
-    msg = "end"
-    client.sendall(msg.encode(FORMAT))
-
 try:
-    client.connect((HOST, SERVER_PORT)) #connect to server you want to
-    print("client address: ", client.getsockname())
+    #glocal variables
+    FOOD_LISTS = []
+    LIST_IMG_LABELS = []
 
-    username = input("username: ")
-    password = input("password: ")
-
-    client.sendall(username.encode(FORMAT)) #because the data send has the type of byte so you need to encode
-    client.recv(1024) #receive message from server, this line of code use to make sure that the above message successfully send to the server before sending the other one
-    client.sendall(password.encode(FORMAT))
+    client.connect((HOST, PORT))
+    msg = "FOOD"
+    client.sendall(msg.encode(FORMAT))
     client.recv(1024)
+    #receive food info from server
+    Food_Info = client.recv(4096)
+    Food_Info = pickle.loads(Food_Info)
+    FOOD_LISTS.append(Food_Info)
+    download_food_image(Food_Info)
+
+    root = Tk()
+    root.option_add( "*font", "Roboto") #set default font
+    root.title("FOOD ORDER APP")
+    root.iconbitmap("icon.ico")
     
-    #simple chat in client section
-    #clientChat(client)
+    #widgets
+    FRAME = LabelFrame(root, padx=50, pady=10)
+    LOGO = ImageTk.PhotoImage(Image.open("logo.png"))
+    LOGO_IMG = Label(FRAME, image=LOGO)
+    WELCOME_LABEL = Label(FRAME, text="Welcome to NNP Restaurant", font=("Roboto", 20, "bold"))
+    BTN_MENU = Button(FRAME, text="Show Food Menu", padx=20,pady=10, command=lambda: show_menu(LOGO_IMG, WELCOME_LABEL, BTN_MENU, client, IMG_LABELS))
+    IMG_LABELS = []
     
-    #how to send a list from client to server
-    clientChat(client)
     
-    #python threading (lap trinh da luong)
+    # img = ImageTk.PhotoImage(Image.open())
+    # label = Label(FRAME, image=img)
+    # IMG_LABELS.append(label)
+    # IMG_LABELS[0].pack()
+    
+    # welcome
+    
+    show_welcome(LOGO_IMG, FRAME, WELCOME_LABEL, BTN_MENU)
+    
+    #put image to label
+    # for idx, info in enumerate(Food_Info):
+        # img = ImageTk.PhotoImage(Image.open("./food_imgs/pic" + str(idx) + ".jpg"))
+        # label = Label(FRAME, image=img)
+        # IMG_LABELS.append(label)
+        
+    
+    img = ImageTk.PhotoImage(Image.open("./food_imgs/pic0" + ".jpg"))
+    label = Label(FRAME, image=img)
+    label.pack()
+        
+    root.eval('tk::PlaceWindow . center')
+    
+    root.mainloop()
+    
     
 except:
-    print("Error")
-
+    print("error")
+    
 input()
+
+
+
+
+
