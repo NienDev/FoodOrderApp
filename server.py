edf5b99126511fdfe5f4d429af371608cffa33a0
NienDev <107404601+NienDev@users.noreply.github.com>
2022-06-23 18:29:15 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,58 +1,32 @@
 import socket
-#192.168.1.20
-HOST = "127.0.0.1" #loopback (point to my laptop IP adđress
-SERVER_PORT = 65432 # > 50000
+import pickle
+import json
+
+HOST = "127.0.0.1"
+PORT = 65432
 FORMAT = "utf8"
 
-def serverChat(conn):
-    msg = None
-    while msg != "x":
-        msg = conn.recv(1024).decode(FORMAT)
-        print("Client Message: ", msg)
-        msg = input("Server message: ")
-        conn.sendall(msg.encode(FORMAT))
-        if msg == "list":
-            list = recvList(conn)
-            print(list)
-        
-def recvList(conn):
-    list = []
-    item = conn.recv(1024).decode(FORMAT)
-    while (item != "end"):
-        list.append(item)
-        conn.sendall(item.encode(FORMAT))
-        item = conn.recv(1024).decode(FORMAT)
-    
-    return list
-    
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #declare socket, SOCK_STREAM: TCP protocol, s used to open and host this server and waiting
-
-s.bind((HOST, SERVER_PORT)) # host on my IP ADDRESS
-s.listen() #waiting 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("SERVER SIDE")
-print("Server:", HOST, SERVER_PORT)
-print("Waiting for client")
 
-#use try ... except means try to connect to something, if something corrupt instead of the error occurs in the terminal, we can skip the try and implement the except
-try: 
-    conn, addr = s.accept() #when client send message, server accept that message, conn used to send and receive data on connect link, addr is address of client
+server.bind((HOST, PORT))
+server.listen()
 
-    print("client address: ", addr)
-    print("conn: ", conn.getsockname())
-
-    username = conn.recv(1024).decode(FORMAT)
-    conn.sendall(username.encode(FORMAT))
-    password = conn.recv(1024).decode(FORMAT)
-    print("Username: ", username)
-    print("Password: ", password)
-    conn.sendall(password.encode(FORMAT))
+try:
+    conn, addr = server.accept()
+    print("client", addr, "has joined")
     
-    #simple char in server section
-    #serverChat(conn)
-    serverChat(conn)
+    msg = conn.recv(1024).decode(FORMAT)
+    conn.sendall(msg.encode(FORMAT))
+    if (msg == "FOOD"):
+        #send FOOD LIST
+        with open('foodData.json') as f:
+            data = json.load(f)
+        print(data)
+        food_list = pickle.dumps(data['food'])
+        conn.sendall(food_list)
 except:
-    print("Error")
+    print("error")
     
 input()
