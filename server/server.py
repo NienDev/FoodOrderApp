a63a71fd1ff89bd4ae7fa5be5e168267bd24c2ab
NienDev <107404601+NienDev@users.noreply.github.com>
2022-07-03 12:00:33 +0700

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,4 +1,62 @@
-ed")
+import socket
+import os
+import pickle
+import json
+
+HOST = "127.0.0.1"
+PORT = 65432
+FORMAT = "utf8"
+
+def sendFile(conn):
+    msg = "FOLDER"
+    conn.sendall(msg.encode(FORMAT))
+    msg = conn.recv(1024).decode(FORMAT)
+    if (msg == "DONE"): return
+    
+    imgs = ["./imgs/thumbnail_banhmi.jpg", "./imgs/thumbnail_bo.jpg", "./imgs/thumbnail_bundau.jpg", "./imgs/thumbnail_banhdau.jpg", "./imgs/thumbnail_comcari.jpg", "./imgs/thumbnail_comga.jpg", "./imgs/thumbnail_dimsum.jpg", "./imgs/thumbnail_goicuon.jpg", "./imgs/thumbnail_mochi.jpg", "./imgs/thumbnail_saladucga.jpg", "./imgs/banhmi.jpg", "./imgs/bo.jpg", "./imgs/bundau.jpg", "./imgs/banhdau.jpg", "./imgs/comcari.jpg", "./imgs/comga.jpg", "./imgs/dimsum.jpg", "./imgs/goicuon.jpg", "./imgs/mochi.jpg", "./imgs/saladucga.jpg", "./imgs/cart.png", "./imgs/icon.ico", "./imgs/logo.png", "./imgs/thank_you.jpg", "./imgs/main_page.jpg"]
+    n = str(len(imgs))
+    conn.sendall(n.encode(FORMAT))
+    for img in imgs:
+        f = open(img, "rb")
+        size_img = os.path.getsize(img)
+        conn.sendall(str(size_img).encode(FORMAT))
+        # print(size_img)
+        data = f.read(size_img)
+        print("sending", img, "to client")
+        conn.sendall(data)
+        print("finish sending", img, "to client")
+        f.close()
+        
+        conn.recv(1024)
+
+def deleteOrder(orderData, newOrder):
+    for idx, order in enumerate(orderData):
+        if (order['ID_Client'] == newOrder['ID_Client']):
+            orderData.pop(idx)
+           
+def sendClientInfo(orderData, addr):
+    global amount_dic
+    for idx, order in enumerate(orderData):
+        if (order['ID_Client'] == addr[0]):
+            for food in order['Food_List']:
+                amount_dic[int(food['Id']) + 1] += 1
+           
+    amount_dic = pickle.dumps(amount_dic)
+    conn.sendall(amount_dic)
+    conn.recv(1024)
+    
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("SERVER SIDE")
+
+server.bind((HOST, PORT))
+server.listen()
+
+# try:
+conn, addr = server.accept()
+amount_dic = [0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+print("client", addr, "has joined")
 
 msg = conn.recv(1024).decode(FORMAT)
 conn.sendall(msg.encode(FORMAT))
